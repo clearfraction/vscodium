bf1ee69cd8f0a9a45f06241825f1ee23e8a797fd
Pavlo Rudy <paulcarroty@users.noreply.github.com>
2021-05-09 17:36:17 +0300

    remove checkout

--- build.sh
+++ build.sh
@@ -56,8 +56,7 @@ echo "Start deployment..."
 git clone -b repos https://gitlab.com/clearfraction/repository.git /tmp/repository
 mv $PWD/.build/linux/rpm/x86_64/rpmbuild/RPMS/x86_64/*.rpm /tmp/repository
 createrepo_c --database --compatibility /tmp/repository
-cd /tmp/repository && git checkout -b repos
-git add .
+cd /tmp/repository && git add .
 git -c user.name='GitlabCI' -c user.email='gitlab@gitlab.com' commit -m 'rebuild the repository'
 git push -f https://paulcarroty:$GITLAB_API_KEY@gitlab.com/clearfraction/repository.git repos
 fi 
